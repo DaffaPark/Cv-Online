f966b9d44252a847a70dfa71926e0e155b88984a
DaffaPark <108559180+DaffaPark@users.noreply.github.com>
2022-09-07 13:14:56 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
     </a>
     <div class="bg-white flex flex-col justify-start p-6">
         <a href="show/{{$post->id}}" class="text-blue-700 text-sm font-bold uppercase pb-4"> KATEGORI </a>
-        <a href="show/{{$post->id}}" class="text-3xl font-bold hover:text-gray-700 pb-4"> {{ $post->judul }} </a>
+        <a href="show/{{$post->id}}" class="text-3xl font-bold hover:text-gray-700 pb-4"> {{ $post->title }} </a>
         <p href="#" class="text-sm pb-3">
             By <a href="#" class="font-semibold hover:text-gray-800">DaffaPark_</a>, Published on {{ date('Y-m-d', strtotime($post->created_at)) }}
         </p>
